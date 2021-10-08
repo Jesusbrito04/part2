3532d38a23e5c3e26405a8578a0674b3a6402fe1
Diego Brito <britodiego.tech@gmail.com>
2021-10-07 23:12:22 -0400

    Finish part2

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Note from "./Note";
 import noteService from "./Service/notes";
+import Notification from "./Service/Notification";
+import Footer from "./Service/footer";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     console.log("Effect");
@@ -26,7 +29,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNoted)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -55,6 +63,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -78,6 +87,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">submit</button>
       </form>
+      <Footer />
     </div>
   );
 };
---
+++ practice/src/Service/footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  const footer = {
+    color: "gray",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footer}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2021
+      </em>
+    </div>
+  );
+};
+
+export default Footer;
